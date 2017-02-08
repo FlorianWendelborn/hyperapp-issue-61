2057c7565f5f18e358c9162594f561a981be1878
Florian Wendelborn <dodekeract@users.noreply.github.com>
2017-02-08 13:20:58 +0100

    Add Root HTML

---
+++ source/index.root.js
@@ -0,0 +1,22 @@
+// region import
+
+import {app, html} from 'hyperapp/hx'
+
+// endregion
+
+// region app
+
+app({
+	view: () => html`
+		<div style=${{
+			position: 'fixed',
+			bottom: '8px',
+			right: '8px',
+			backgroundColor: 'yellow'
+		}}>
+			View
+		</div>`,
+	root: document.getElementById('hyperapp')
+})
+
+// endregion
